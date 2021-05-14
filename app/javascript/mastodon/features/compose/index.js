mirielnet/rdon

Add place tab bar at bottom and add short label

app/javascript/mastodon/features/compose/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ComposeFormContainer from './containers/compose_form_container';
 import NavigationContainer from './containers/navigation_container';
 import PropTypes from 'prop-types';
@@ -14,11 +14,20 @@ import SearchResultsContainer from './containers/search_results_container';
 import { changeComposing } from '../../actions/compose';
 import { openModal } from 'mastodon/actions/modal';
 import elephantUIPlane from '../../../images/elephant_ui_plane.svg';
-import { mascot } from '../../initial_state';
+import { mascot, show_tab_bar_label } from '../../initial_state';
 import Icon from 'mastodon/components/icon';
 import { logOut } from 'mastodon/utils/log_out';
+import NotificationsCounterIcon from '../ui/components/notifications_counter_icon';
+import classNames from 'classnames';
 
 const messages = defineMessages({
+  short_start: { id: 'navigation_bar.short.getting_started', defaultMessage: 'Started' },
+  short_home_timeline: { id: 'navigation_bar.short.home', defaultMessage: 'Home' },
+  short_notifications: { id: 'navigation_bar.short.notifications', defaultMessage: 'Notif.' },
+  short_public: { id: 'navigation_bar.short.public_timeline', defaultMessage: 'FTL' },
+  short_community: { id: 'navigation_bar.short.community_timeline', defaultMessage: 'LTL' },
+  short_preferences: { id: 'navigation_bar.short.preferences', defaultMessage: 'Pref.' },
+  short_logout: { id: 'navigation_bar.short.logout', defaultMessage: 'Logout' },
   start: { id: 'getting_started.heading', defaultMessage: 'Getting started' },
   home_timeline: { id: 'tabs_bar.home', defaultMessage: 'Home' },
   notifications: { id: 'tabs_bar.notifications', defaultMessage: 'Notifications' },
@@ -88,30 +97,52 @@ class Compose extends React.PureComponent {
     this.props.dispatch(changeComposing(false));
   }
 
+  tab (id) {
+    const { columns, intl: { formatMessage } } = this.props;
+
+    if (!columns.some(column => column.get('id') === id)) {
+      const tabParams = {
+        'START':         { to: '/getting-started',        title: formatMessage(messages.start),         label: formatMessage(messages.short_start),         icon_id: 'bars' },
+        'HOME':          { to: '/timelines/home',         title: formatMessage(messages.home_timeline), label: formatMessage(messages.short_home_timeline), icon_id: 'home' },
+        'NOTIFICATIONS': { to: '/notifications',          title: formatMessage(messages.notifications), label: formatMessage(messages.short_notifications), icon_id: 'bell' },
+        'COMMUNITY':     { to: '/timelines/public/local', title: formatMessage(messages.community),     label: formatMessage(messages.short_community),     icon_id: 'users' },
+        'PUBLIC':        { to: '/timelines/public',       title: formatMessage(messages.public),        label: formatMessage(messages.short_public),        icon_id: 'globe' },
+        'PREFERENCES':   { href: '/settings/preferences', title: formatMessage(messages.preferences),   label: formatMessage(messages.short_preferences),   icon_id: 'cog' },
+        'SIGN_OUT':      { href: '/auth/sign_out',        title: formatMessage(messages.logout),        label: formatMessage(messages.short_logout),        icon_id: 'sign-out' },
+      };
+
+      const { href, to, title, label, icon_id } = tabParams[id];
+
+      const icon = (id === 'NOTIFICATIONS') ? <NotificationsCounterIcon /> : <Icon id={icon_id} fixedWidth />;
+
+      if (href) {
+        return (
+          <a href={href} className={classNames('drawer__tab', { 'short-label': show_tab_bar_label })} title={title} aria-label={title}>{icon}<span className='drawer__tab__short-label'>{label}</span></a>
+        );
+      } else {
+        return (
+          <Link to={to} className={classNames('drawer__tab', { 'short-label': show_tab_bar_label })} title={title} aria-label={title}>{icon}<span className='drawer__tab__short-label'>{label}</span></Link>
+        );
+      }
+    }
+    return null;
+  }
+
   render () {
     const { multiColumn, showSearch, isSearchPage, intl } = this.props;
 
     let header = '';
 
     if (multiColumn) {
-      const { columns } = this.props;
+      const defaultTabIds = ['START', 'HOME', 'NOTIFICATIONS', 'COMMUNITY', 'PUBLIC', 'PREFERENCES', 'SIGN_OUT'];
+
+      let tabs = defaultTabIds;
+
       header = (
         <nav className='drawer__header'>
-          <Link to='/getting-started' className='drawer__tab' title={intl.formatMessage(messages.start)} aria-label={intl.formatMessage(messages.start)}><Icon id='bars' fixedWidth /></Link>
-          {!columns.some(column => column.get('id') === 'HOME') && (
-            <Link to='/timelines/home' className='drawer__tab' title={intl.formatMessage(messages.home_timeline)} aria-label={intl.formatMessage(messages.home_timeline)}><Icon id='home' fixedWidth /></Link>
-          )}
-          {!columns.some(column => column.get('id') === 'NOTIFICATIONS') && (
-            <Link to='/notifications' className='drawer__tab' title={intl.formatMessage(messages.notifications)} aria-label={intl.formatMessage(messages.notifications)}><Icon id='bell' fixedWidth /></Link>
-          )}
-          {!columns.some(column => column.get('id') === 'COMMUNITY') && (
-            <Link to='/timelines/public/local' className='drawer__tab' title={intl.formatMessage(messages.community)} aria-label={intl.formatMessage(messages.community)}><Icon id='users' fixedWidth /></Link>
-          )}
-          {!columns.some(column => column.get('id') === 'PUBLIC') && (
-            <Link to='/timelines/public' className='drawer__tab' title={intl.formatMessage(messages.public)} aria-label={intl.formatMessage(messages.public)}><Icon id='globe' fixedWidth /></Link>
-          )}
-          <a href='/settings/preferences' className='drawer__tab' title={intl.formatMessage(messages.preferences)} aria-label={intl.formatMessage(messages.preferences)}><Icon id='cog' fixedWidth /></a>
-          <a href='/auth/sign_out' className='drawer__tab' title={intl.formatMessage(messages.logout)} aria-label={intl.formatMessage(messages.logout)} onClick={this.handleLogoutClick}><Icon id='sign-out' fixedWidth /></a>
+          {tabs.map(tabId => (
+            <Fragment key={tabId}>{this.tab(tabId)}</Fragment>
+          ))}
         </nav>
       );
     }

Y-zu-don-maintenance-org/mastodon

	modified:   app/controllers/settings/preferences_controller.rb
	modified:   app/javascript/mastodon/features/compose/index.js
	modified:   app/javascript/mastodon/features/ui/components/columns_area.js
	modified:   app/javascript/mastodon/features/ui/components/tabs_bar.js
	modified:   app/javascript/mastodon/initial_state.js
	modified:   app/javascript/mastodon/locales/en.json
	modified:   app/javascript/mastodon/locales/ja.json
	modified:   app/javascript/styles/mastodon/basics.scss
	modified:   app/javascript/styles/mastodon/components.scss
	modified:   app/lib/user_settings_decorator.rb
	modified:   app/models/user.rb
	modified:   app/serializers/initial_state_serializer.rb
	modified:   app/views/layouts/application.html.haml
	modified:   app/views/layouts/error.html.haml
	modified:   app/views/settings/preferences/other/show.html.haml
	modified:   config/locales/simple_form.en.yml
	modified:   config/locales/simple_form.ja.yml
	modified:   config/settings.yml

app/javascript/mastodon/features/compose/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ComposeFormContainer from './containers/compose_form_container';
 import NavigationContainer from './containers/navigation_container';
 import PropTypes from 'prop-types';
@@ -14,12 +14,22 @@ import SearchResultsContainer from './containers/search_results_container';
 import { changeComposing } from '../../actions/compose';
 import { openModal } from 'mastodon/actions/modal';
 import elephantUIPlane from '../../../images/elephant_ui_plane.svg';
-import { mascot } from '../../initial_state';
+import { mascot, show_tab_bar_label } from '../../initial_state';
 import Icon from 'mastodon/components/icon';
 import { logOut } from 'mastodon/utils/log_out';
 import AnnouncementsContainer from './containers/announcements_container';
+import NotificationsCounterIcon from '../ui/components/notifications_counter_icon';
+import classNames from 'classnames';
 
 const messages = defineMessages({
+  short_start: { id: 'navigation_bar.short.getting_started', defaultMessage: 'Started' },
+  short_home_timeline: { id: 'navigation_bar.short.home', defaultMessage: 'Home' },
+  short_notifications: { id: 'navigation_bar.short.notifications', defaultMessage: 'Notif.' },
+  short_public: { id: 'navigation_bar.short.public_timeline', defaultMessage: 'FTL' },
+  short_community: { id: 'navigation_bar.short.community_timeline', defaultMessage: 'LTL' },
+  short_lists: { id: 'navigation_bar.short.lists', defaultMessage: 'Lists' },
+  short_preferences: { id: 'navigation_bar.short.preferences', defaultMessage: 'Pref.' },
+  short_logout: { id: 'navigation_bar.short.logout', defaultMessage: 'Logout' },
   start: { id: 'getting_started.heading', defaultMessage: 'Getting started' },
   home_timeline: { id: 'tabs_bar.home', defaultMessage: 'Home' },
   notifications: { id: 'tabs_bar.notifications', defaultMessage: 'Notifications' },
@@ -89,30 +99,54 @@ class Compose extends React.PureComponent {
     this.props.dispatch(changeComposing(false));
   }
 
+  tab (id) {
+    const { columns, intl: { formatMessage } } = this.props;
+
+    if (!columns.some(column => column.get('id') === id)) {
+      const tabParams = {
+        'START':         { to: '/getting-started',        title: formatMessage(messages.start),         label: formatMessage(messages.short_start),         icon_id: 'bars' },
+        'HOME':          { to: '/timelines/home',         title: formatMessage(messages.home_timeline), label: formatMessage(messages.short_home_timeline), icon_id: 'home' },
+        'NOTIFICATIONS': { to: '/notifications',          title: formatMessage(messages.notifications), label: formatMessage(messages.short_notifications), icon_id: 'bell' },
+        'COMMUNITY':     { to: '/timelines/public/local', title: formatMessage(messages.community),     label: formatMessage(messages.short_community),     icon_id: 'users' },
+        'PUBLIC':        { to: '/timelines/public',       title: formatMessage(messages.public),        label: formatMessage(messages.short_public),        icon_id: 'globe' },
+        'LIST':          { to: '/lists',                  title: formatMessage(messages.lists),         label: formatMessage(messages.short_lists),         icon_id: 'list-ul' },
+        'PREFERENCES':   { href: '/settings/preferences', title: formatMessage(messages.preferences),   label: formatMessage(messages.short_preferences),   icon_id: 'cog' },
+        'SIGN_OUT':      { href: '/auth/sign_out',        title: formatMessage(messages.logout),        label: formatMessage(messages.short_logout),        icon_id: 'sign-out' },
+      };
+
+      const { href, to, title, label, icon_id } = tabParams[id];
+
+      const icon = (id === 'NOTIFICATIONS') ? <NotificationsCounterIcon /> : <Icon id={icon_id} fixedWidth />;
+
+      if (href) {
+        return (
+          <a href={href} className={classNames('drawer__tab', { 'short-label': show_tab_bar_label })} title={title} aria-label={title}>{icon}<span className='drawer__tab__short-label'>{label}</span></a>
+        );
+      } else {
+        return (
+          <Link to={to} className={classNames('drawer__tab', { 'short-label': show_tab_bar_label })} title={title} aria-label={title}>{icon}<span className='drawer__tab__short-label'>{label}</span></Link>
+        );
+      }
+    }
+    return null;
+  }
+
   render () {
     const { multiColumn, showSearch, isSearchPage, intl } = this.props;
 
     let header = '';
 
     if (multiColumn) {
-      const { columns } = this.props;
+      // const defaultTabIds = ['START', 'HOME', 'NOTIFICATIONS', 'COMMUNITY', 'PUBLIC', 'LIST', 'PREFERENCES', 'SIGN_OUT'];
+      const defaultTabIds = ['START', 'HOME', 'NOTIFICATIONS', 'PUBLIC', 'LIST', 'PREFERENCES', 'SIGN_OUT'];
+
+      let tabs = defaultTabIds;
+
       header = (
         <nav className='drawer__header'>
-          <Link to='/getting-started' className='drawer__tab' title={intl.formatMessage(messages.start)} aria-label={intl.formatMessage(messages.start)}><Icon id='bars' fixedWidth /></Link>
-          {!columns.some(column => column.get('id') === 'HOME') && (
-            <Link to='/timelines/home' className='drawer__tab' title={intl.formatMessage(messages.home_timeline)} aria-label={intl.formatMessage(messages.home_timeline)}><Icon id='home' fixedWidth /></Link>
-          )}
-          {!columns.some(column => column.get('id') === 'NOTIFICATIONS') && (
-            <Link to='/notifications' className='drawer__tab' title={intl.formatMessage(messages.notifications)} aria-label={intl.formatMessage(messages.notifications)}><Icon id='bell' fixedWidth /></Link>
-          )}
-          {!columns.some(column => column.get('id') === 'COMMUNITY') && (
-            <Link to='/timelines/public/local' className='drawer__tab' title={intl.formatMessage(messages.community)} aria-label={intl.formatMessage(messages.community)}><Icon id='users' fixedWidth /></Link>
-          )}
-          {!columns.some(column => column.get('id') === 'PUBLIC') && (
-            <Link to='/timelines/public' className='drawer__tab' title={intl.formatMessage(messages.public)} aria-label={intl.formatMessage(messages.public)}><Icon id='globe' fixedWidth /></Link>
-          )}
-          <a href='/settings/preferences' className='drawer__tab' title={intl.formatMessage(messages.preferences)} aria-label={intl.formatMessage(messages.preferences)}><Icon id='cog' fixedWidth /></a>
-          <a href='/auth/sign_out' className='drawer__tab' title={intl.formatMessage(messages.logout)} aria-label={intl.formatMessage(messages.logout)} onClick={this.handleLogoutClick}><Icon id='sign-out' fixedWidth /></a>
+          {tabs.map(tabId => (
+	    <Fragment key={tabId}>{this.tab(tabId)}</Fragment>
+          ))}
         </nav>
       );
     }