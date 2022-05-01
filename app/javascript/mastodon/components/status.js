32be19d6f1e3dd6247b308cf6f7a93d8508c38f5
YoheiZuho <yuuman002@gmail.com>
2022-05-01 19:37:47 +0900

    fix if click name in QT can't show post owner's profile(8)

--- app/javascript/mastodon/components/status.js
+++ app/javascript/mastodon/components/status.js
@@ -186,12 +186,11 @@ class Status extends ImmutablePureComponent {
   }
 
   handleAccountClick = (e, proper = true) => {      
-    // if (this.context.router && e.button === 0 && !(e.ctrlKey || e.metaKey)) {
-    //   const id = e.currentTarget.getAttribute('data-id');
-    //   e.preventDefault();
-    //   this.context.router.history.push(`/accounts/${id}`);
-    // }
-    this._openProfile(proper);
+    if (this.context.router && e.button === 0 && !(e.ctrlKey || e.metaKey)) {
+      const id = e.currentTarget.getAttribute('data-id');
+      e.preventDefault();
+      this._openProfile(proper);
+    }
   }
 
   handleQuoteClick = () => {
